ef32f3046b9ae962b7c4be27d35e059ed1964ac4
eight2018 <gustavolaurenzano@hotmail.com>
2019-05-10 17:19:13 -0300

    Add files via upload

---
+++ qqq/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+
+    char letra= 'e';
+    char carga[5];
+    //char letra2='c';
+    printf("Hello world!\n");
+
+    for(int i=0; i<5 ;i++){
+      carga[i]=letra;
+    }
+     for(int i=0; i<5 ;i++){
+      printf("%c", carga[i]) ;
+    }
+ float num=0-12;
+ printf("%.1f ",num);
+
+
+
+    return 0;
+}
